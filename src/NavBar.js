480690be628ab25ebde4461f972577bbf2fee080
Housni <alami.housni@gmail.com>
2024-02-09 13:31:39 +0100

    navbar links layed out

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,7 +1,28 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 
 function NavBar() {
-    return "hello"
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">NOS CUISINES</Nav.Link>
+                        <Nav.Link href="#link">QUI SOMME-NOUS</Nav.Link>
+                        <Nav.Link href="#link">CONTACTEZ NOUS</Nav.Link>
+                        <Nav.Link href="#link">NOS CLIENTS</Nav.Link>
+                        <Nav.Link href="#link">SHOWROOM</Nav.Link>
+                        <Nav.Link href="#link">FAQs</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
-export default NavBar
+export default NavBar;

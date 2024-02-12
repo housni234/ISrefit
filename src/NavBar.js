162a62314cff54744fa53518a16491c248cba817
Housni <alami.housni@gmail.com>
2024-02-12 14:16:58 +0100

    NavBar grid setting up in progress

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,25 +1,46 @@
 import React from "react";
+import "./App.css"
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Image from 'react-bootstrap/Image';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+
+
 
 
 function NavBar() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">NOS CUISINES</Nav.Link>
-                        <Nav.Link href="#link">QUI SOMME-NOUS</Nav.Link>
-                        <Nav.Link href="#link">CONTACTEZ NOUS</Nav.Link>
-                        <Nav.Link href="#link">NOS CLIENTS</Nav.Link>
-                        <Nav.Link href="#link">SHOWROOM</Nav.Link>
-                        <Nav.Link href="#link">FAQs</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+            <Container fluid>
+                <Row>
+                    <Col >
+                        <Image className="logo" src="holder.js/171x180" thumbnail />
+                    </Col>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Col xs={3}>
+                                <Nav.Link href="#home">NOS CUISINES</Nav.Link>
+                            </Col>
+                            <Col xs={3}>
+                                <Nav.Link href="#link">QUI SOMME-NOUS</Nav.Link>
+                            </Col>
+                            <Col xs={3}>
+                                <Nav.Link href="#link">CONTACTEZ NOUS</Nav.Link>
+                            </Col>
+                            <Col xs={3}>
+                                <Nav.Link href="#link">NOS CLIENTS</Nav.Link>
+                            </Col>
+                            <Col xs={3}>
+                                <Nav.Link href="#link">SHOWROOM</Nav.Link>
+                            </Col>
+                            <Nav.Link href="#link">FAQs</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Row>
             </Container>
         </Navbar>
     );

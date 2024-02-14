6344f67250ab811328dfa1179fb6d761a25f28be
Housni <alami.housni@gmail.com>
2024-02-14 13:46:47 +0100

    link content adjusted to width-auto

--- src/NavBar.js
+++ src/NavBar.js
@@ -14,35 +14,35 @@ import Row from 'react-bootstrap/Row';
 function NavBar() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
-            <Container fluid>
-                <Row>
-                    <Col >
-                        <Image className="logo" src="holder.js/171x180" thumbnail />
-                    </Col>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Col xs={3}>
+            <Container >
+                <Image className="logo" src="holder.js/171x180" thumbnail />
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Row >
+                            <Col md="auto">
                                 <Nav.Link href="#home">NOS CUISINES</Nav.Link>
                             </Col>
-                            <Col xs={3}>
+                            <Col md="auto" >
                                 <Nav.Link href="#link">QUI SOMME-NOUS</Nav.Link>
                             </Col>
-                            <Col xs={3}>
+                            <Col md="auto">
                                 <Nav.Link href="#link">CONTACTEZ NOUS</Nav.Link>
                             </Col>
-                            <Col xs={3}>
+                            <Col md="auto">
                                 <Nav.Link href="#link">NOS CLIENTS</Nav.Link>
                             </Col>
-                            <Col xs={3}>
+                            <Col>
                                 <Nav.Link href="#link">SHOWROOM</Nav.Link>
                             </Col>
-                            <Nav.Link href="#link">FAQs</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Row>
+                            <Col>
+                                <Nav.Link href="#link">FAQs</Nav.Link>
+                            </Col>
+                        </Row>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
-        </Navbar>
+        </Navbar >
     );
 }
 

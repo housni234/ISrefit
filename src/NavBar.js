4aee5d029c1de58a2d1ae59b9022dcfa31efae81
Housni <alami.housni@gmail.com>
2024-03-16 12:52:13 +0100

    Nav.link centered successfully

--- src/NavBar.js
+++ src/NavBar.js
@@ -14,16 +14,16 @@ import Row from 'react-bootstrap/Row';
 function NavBar() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
-            <Container fluid >
+            <Container fluid>
                 <Image className="logo" src="holder.js/171x180" thumbnail />
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto" >
-                        <Row className="row">
+                    <Nav className="mx-auto" >
+                        <Row>
                             <Col md="auto">
                                 <Nav.Link href="#home" className="Navlink">NOS CUISINES</Nav.Link>
                             </Col>
-                            <Col md="auto" >
+                            <Col md="auto">
                                 <Nav.Link href="#link" className="Navlink">QUI SOMME-NOUS</Nav.Link>
                             </Col>
                             <Col md="auto">
@@ -43,6 +43,28 @@ function NavBar() {
                 </Navbar.Collapse>
             </Container>
         </Navbar >
+        // <Container fluid>
+        //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        //     <Image className="logo" src="holder.js/171x180" thumbnail />
+        //     <Navbar.Collapse id="basic-navbar-nav">
+        //         <Nav className="me-auto" >
+        //             <Row className="justify-content-md-center">
+        //                 <Col md="auto">
+        //                     1 of 3
+        //                 </Col>
+        //                 <Col md="auto">Var</Col>
+        //                 <Col md="auto">
+        //                     3 of 3
+        //                 </Col>
+        //                 <Col md="auto">1 of 3</Col>
+        //                 <Col md="auto">Vari</Col>
+        //                 <Col md="auto">
+        //                     3 of 3
+        //                 </Col>
+        //             </Row>
+        //         </Nav>
+        //     </Navbar.Collapse>
+        // </Container >
     );
 }
 

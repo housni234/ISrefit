d613bca27acd22e4b420fd872aae0f6d0b3a3283
Housni <alami.housni@gmail.com>
2024-03-28 17:21:39 +0100

    forms layout done

--- src/Components/Formulaire.js
+++ src/Components/Formulaire.js
@@ -8,44 +8,54 @@ import Col from 'react-bootstrap/Col';
 
 function Formulaire() {
     return (
-        <Form>
-            <h2 className='d-flex justify-content-center'>Envoyez nous une demande de consultation</h2>
-            <Container>
-                <Row xs="auto" md="auto" lg="auto">
-                    <Col >
-                        <Form.Control placeholder="Nom" />
-                    </Col>
-                    <Col>
-                        <Form.Control placeholder="Prenom" />
-                    </Col>
-                    <Col>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Control type="email" placeholder="Email" />
-                        </Form.Group>
-                    </Col>
-                    <Col>
-                        <Form.Group as={Col} controlId="formGridState" >
-                            <Form.Select defaultValue="Choose...">
-                                <option>Pays</option>
-                                <option>...</option>
-                            </Form.Select>
-                        </Form.Group></Col>
-                    <Col>
-                        <Col>
+        <div>
+            <h2 className='d-flex justify-content-center p-4 mt-4'>Envoyez nous une demande de consultation</h2>
+            <Form>
+                <Container>
+                    <Row xs="auto" md="auto" lg="auto">
+                        <Col className='' >
+                            <Form.Control placeholder="Nom" />
+                        </Col>
+                        <Col className='p-0'>
+                            <Form.Control placeholder="Prenom" />
+                        </Col>
+                        <Col className=''>
                             <Form.Group as={Col} controlId="formGridState" >
                                 <Form.Select defaultValue="Choose...">
-                                    <option>Ville</option>
+                                    <option>Pays</option>
                                     <option>...</option>
                                 </Form.Select>
                             </Form.Group></Col>
-                        <Col></Col>
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Col>
-                </Row>
-            </Container>
-        </Form>
+                        <Col className='p-0'>
+                            <Form.Group as={Col} controlId="formGridState" >
+                                <Form.Select defaultValue="Choose...">
+                                    <option>Ville</option>
+                                </Form.Select>
+                            </Form.Group>
+                        </Col>
+                        <Col className=''>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Control type="email" placeholder="Email" />
+                                <div id="passwordHelpBlock" class="form-text">Optionnel</div>
+                            </Form.Group>
+                        </Col>
+                        <Col className='p-0'>
+                            <Form.Group as={Col} controlId="formGridState" >
+                                <Form.Select defaultValue="Choose...">
+                                    <option>Telephone</option>
+                                </Form.Select>
+                            </Form.Group>
+                        </Col>
+
+                        <Col>
+                            <Button variant="primary" type="submit" style={{ backgroundColor: "rgba(255, 188, 16, 1)", color: "black", border: "1px solid rgba(206, 206, 206, 1)" }}>
+                                Consulter
+                            </Button>
+                        </Col>
+                    </Row>
+                </Container>
+            </Form >
+        </div>
     );
 }
 

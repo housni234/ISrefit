f4b49a2696b8739ec5c75f8f17f6fcb9400a0f03
Housni Alami <alami.housni@gmail.com>
2024-07-24 14:15:51 +0200

    nos clients component done

--- src/Components/NosClients.js
+++ src/Components/NosClients.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col, Card, Image } from "react-bootstrap";
 
 function NosClients() {
     return (<>
@@ -10,8 +10,14 @@ function NosClients() {
                 </Col>
             </Row>
             <Row className="cards-row" >
-                <Col className="mx-auto d-block m-4 b-4" lg="3">
+                <Col className="mx-auto d-block m-4 b-4 " lg="3">
                     <Card className="card" >
+                        <div className="d-flex justify-content-center m-2">
+                            <Image src={require("../images/stars.jpg")} className="stars" />
+                        </div>
+                        <Card.Title className=" m-2 card-title">
+                            Housni Alami
+                        </Card.Title>
                         <Card.Text className="p-3">
                             honrfonefono oi o oowoue   oerh iug  ywg iwg  wiur giweur  piwuriuwe r weiuyrpiuwy iuuwy iuwyr
                             pouwy thwkfsg  kh hoehr;ewh rohkrjhf herklherkjgherkg erk kwhkjfhg kjh ewkhrflk wejh kwejh weh
@@ -20,6 +26,12 @@ function NosClients() {
                 </Col>
                 <Col className="mx-auto d-block m-4" lg="3">
                     <Card className="card">
+                        <div className="d-flex justify-content-center m-2">
+                            <Image src={require("../images/stars.jpg")} className="stars" />
+                        </div>
+                        <Card.Title className="m-2 card-title">
+                            Housni Alami
+                        </Card.Title>
                         <Card.Text className="p-3">
                             honrfonefono honrfonefono oi o oowoue   oerh iug  ywg iwg  wiur giweur  piwuriuwe r weiuyrpiuwy iuuwy iuwyr
                             pouwy thwkfsg  kh hoehr;ewh rohkrjhf herklherkjgherkg erk kwhkjfhg kjh ewkhrflk wejh kwejh weh
@@ -28,6 +40,12 @@ function NosClients() {
                 </Col>
                 <Col className="mx-auto d-block m-4" lg="3">
                     <Card className="card">
+                        <div className="d-flex justify-content-center m-2">
+                            <Image src={require("../images/stars.jpg")} className="stars" />
+                        </div>
+                        <Card.Title className="m-2 card-title">
+                            Housni Alami
+                        </Card.Title>
                         <Card.Text className="p-3">
                             honrfonefono honrfonefono oi o oowoue   oerh iug  ywg iwg  wiur giweur  piwuriuwe r weiuyrpiuwy iuuwy iuwyr
                             pouwy thwkfsg  kh hoehr;ewh rohkrjhf herklherkjgherkg erk kwhkjfhg kjh ewkhrflk wejh kwejh weh

5c582651518d46b597de9bf5e4851935905bc6a8
Housni Alami <alami.housni@gmail.com>
2024-07-24 17:26:47 +0200

    adding loadmore amd showless buttons to nos client component

--- src/Components/NosClients.js
+++ src/Components/NosClients.js
@@ -1,8 +1,33 @@
-import React from "react";
-import { Container, Row, Col, Card, Image } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Card, Image, Button } from "react-bootstrap";
 
 function NosClients() {
-    return (<>
+    //  data for cards
+    const clients = [
+        { id: 1, name: "Housni Alami", imgSrc: require("../images/stars.jpg"), text: "honrfonefono oi o oowoue oerh iug  ywg iwg  wiur giweur  piwuriuwe r weiuyrpiuwy iuuwy iuwyrpouwy thwkfsg  kh hoehr;ewh rohkrjhf herklherkjgherkg erk kwhkjfhg kjh ewkhrflk wejh kwejh weh" },
+        { id: 2, name: "Housni Alami", imgSrc: require("../images/stars.jpg"), text: "honrfonefono oi o oowoue oerh iug  ywg iwg  wiur giweur  piwuriuwe r weiuyrpiuwy iuuwy iuwyrpouwy thwkfsg  kh hoehr;ewh rohkrjhf herklherkjgherkg erk kwhkjfhg kjh ewkhrflk wejh kwejh weh" },
+        { id: 3, name: "Housni Alami", imgSrc: require("../images/stars.jpg"), text: "honrfonefono oi o oowoue oerh iug  ywg iwg  wiur giweur  piwuriuwe r weiuyrpiuwy iuuwy iuwyrpouwy thwkfsg  kh hoehr;ewh rohkrjhf herklherkjgherkg erk kwhkjfhg kjh ewkhrflk wejh kwejh weh" },
+        { id: 4, name: "Housni Alami", imgSrc: require("../images/stars.jpg"), text: "honrfonefono oi o oowoue oerh iug  ywg iwg  wiur giweur  piwuriuwe r weiuyrpiuwy iuuwy iuwyrpouwy thwkfsg  kh hoehr;ewh rohkrjhf herklherkjgherkg erk kwhkjfhg kjh ewkhrflk wejh kwejh weh" },
+        { id: 5, name: "Housni Alami", imgSrc: require("../images/stars.jpg"), text: "honrfonefono oi o oowoue oerh iug  ywg iwg  wiur giweur  piwuriuwe r weiuyrpiuwy iuuwy iuwyrpouwy thwkfsg  kh hoehr;ewh rohkrjhf herklherkjgherkg erk kwhkjfhg kjh ewkhrflk wejh kwejh weh" },
+        { id: 6, name: "Housni Alami", imgSrc: require("../images/stars.jpg"), text: "honrfonefono oi o oowoue oerh iug  ywg iwg  wiur giweur  piwuriuwe r weiuyrpiuwy iuuwy iuwyrpouwy thwkfsg  kh hoehr;ewh rohkrjhf herklherkjgherkg erk kwhkjfhg kjh ewkhrflk wejh kwejh weh" },
+
+    ];
+
+    const intialVisibleCardsState = 3;
+    const [visibleCards, setVisibleCards] = useState(intialVisibleCardsState);
+
+   
+
+    const loadMore = () => {
+        setVisibleCards((preVisibleCards) => preVisibleCards + 3);
+    }
+
+    const showLess = () => {
+        setVisibleCards(intialVisibleCardsState)
+    }
+
+
+    return (
         <Container >
             <Row className="justify-content-center mt-5">
                 <Col className='text-center mt-5'>
@@ -10,52 +35,31 @@ function NosClients() {
                 </Col>
             </Row>
             <Row className="cards-row" >
-                <Col className="mx-auto d-block m-4 b-4 " lg="3">
-                    <Card className="card" >
-                        <div className="d-flex justify-content-center m-2">
-                            <Image src={require("../images/stars.jpg")} className="stars" />
-                        </div>
-                        <Card.Title className=" m-2 card-title">
-                            Housni Alami
-                        </Card.Title>
-                        <Card.Text className="p-3">
-                            honrfonefono oi o oowoue   oerh iug  ywg iwg  wiur giweur  piwuriuwe r weiuyrpiuwy iuuwy iuwyr
-                            pouwy thwkfsg  kh hoehr;ewh rohkrjhf herklherkjgherkg erk kwhkjfhg kjh ewkhrflk wejh kwejh weh
-                        </Card.Text>
-                    </Card>
-                </Col>
-                <Col className="mx-auto d-block m-4" lg="3">
-                    <Card className="card">
-                        <div className="d-flex justify-content-center m-2">
-                            <Image src={require("../images/stars.jpg")} className="stars" />
-                        </div>
-                        <Card.Title className="m-2 card-title">
-                            Housni Alami
-                        </Card.Title>
-                        <Card.Text className="p-3">
-                            honrfonefono honrfonefono oi o oowoue   oerh iug  ywg iwg  wiur giweur  piwuriuwe r weiuyrpiuwy iuuwy iuwyr
-                            pouwy thwkfsg  kh hoehr;ewh rohkrjhf herklherkjgherkg erk kwhkjfhg kjh ewkhrflk wejh kwejh weh
-                        </Card.Text>
-                    </Card>
-                </Col>
-                <Col className="mx-auto d-block m-4" lg="3">
-                    <Card className="card">
-                        <div className="d-flex justify-content-center m-2">
-                            <Image src={require("../images/stars.jpg")} className="stars" />
-                        </div>
-                        <Card.Title className="m-2 card-title">
-                            Housni Alami
-                        </Card.Title>
-                        <Card.Text className="p-3">
-                            honrfonefono honrfonefono oi o oowoue   oerh iug  ywg iwg  wiur giweur  piwuriuwe r weiuyrpiuwy iuuwy iuwyr
-                            pouwy thwkfsg  kh hoehr;ewh rohkrjhf herklherkjgherkg erk kwhkjfhg kjh ewkhrflk wejh kwejh weh
-                        </Card.Text>
-                    </Card>
-                </Col>
+                {clients.slice(0, visibleCards).map((client) => (
+                    <Col className="mx-auto d-block m-4 b-4 " lg="4">
+                        <Card className="card" >
+                            <div className="d-flex justify-content-center m-2">
+                                <Image src={client.imgSrc} className="stars" />
+                            </div>
+                            <Card.Title className=" m-2 card-title">
+                                {client.name}
+                            </Card.Title>
+                            <Card.Text className="m-4 card-text">
+                                {client.text}
+                            </Card.Text>
+                        </Card>
+                    </Col>
+                ))}
+                <Row className="justify-content-center">
+                    {visibleCards < clients.length && (
+                        <Button onClick={loadMore} className="mb-2 button">Load More</Button>
+                    )}
+                    {visibleCards > intialVisibleCardsState && (
+                        <Button onClick={showLess} className="mb-2 ml-2 button">Show Less</Button>
+                    )}
+                </Row>
             </Row>
         </Container>
-
-    </>
     )
 }
 
